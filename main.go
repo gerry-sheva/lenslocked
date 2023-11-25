d688800a1b2af524f7a90bf525863de1d9c472d8
gerry-sheva <gerry.sheva@proton.me>
2023-11-25 15:15:09 +0700

    used chi

--- main.go
+++ main.go
@@ -3,6 +3,8 @@ package main
 import (
 	"fmt"
 	"net/http"
+
+	"github.com/go-chi/chi/v5"
 )
 
 func homeHandler(w http.ResponseWriter, r *http.Request) {
@@ -10,13 +12,14 @@ func homeHandler(w http.ResponseWriter, r *http.Request) {
 }
 
 func contactHandler(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprint(w, "<h1>Contact Pge</h1><p>You can contact me at sheva@mail.com</p>")
+	fmt.Fprint(w, "<h1>Contact Page</h1><p>You can contact me at sheva@mail.com</p>")
 }
 
 func main() {
-	http.HandleFunc("/", homeHandler)
-	http.HandleFunc("/contact", contactHandler)
+	r := chi.NewRouter()
+	r.Get("/", homeHandler)
+	r.Get("/contact", contactHandler)
 
 	fmt.Println("server is ready at port 3000")
-	http.ListenAndServe(":3000", nil)
+	http.ListenAndServe(":3000", r)
 }
