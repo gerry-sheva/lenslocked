fd5c62d148dc796ac46c2fce969b82b214ff6670
gerry-sheva <gerry.sheva@proton.me>
2023-11-25 23:12:05 +0700

    added template rendering

--- main.go
+++ main.go
@@ -2,23 +2,51 @@ package main
 
 import (
 	"fmt"
+	"html/template"
+	"log"
 	"net/http"
 
 	"github.com/go-chi/chi/v5"
+	"github.com/go-chi/chi/v5/middleware"
 )
 
+func executeTemplate(w http.ResponseWriter, filepath string) {
+	w.Header().Set("Content-Type", "text/html; charset=utf-8")
+	tmpl, err := template.ParseFiles(filepath)
+	if err != nil {
+		log.Printf("Parsing error: %v", err)
+		http.Error(w, "There was an errror during parsing", http.StatusInternalServerError)
+		return
+	}
+	err = tmpl.Execute(w, nil)
+	if err != nil {
+		log.Printf("Executing template error: %v", err)
+		http.Error(w, "There was an error during execution", http.StatusInternalServerError)
+		return
+	}
+}
+
 func homeHandler(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprint(w, "Welcome to my Home Page")
+	tmplPath := "templates/home.gohtml"
+	executeTemplate(w, tmplPath)
 }
 
 func contactHandler(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprint(w, "<h1>Contact Page</h1><p>You can contact me at sheva@mail.com</p>")
+	tmplPath := "templates/contact.gohtml"
+	executeTemplate(w, tmplPath)
+}
+
+func faqHandler(w http.ResponseWriter, r *http.Request) {
+	tmplPath := "templates/faq.gohtml"
+	executeTemplate(w, tmplPath)
 }
 
 func main() {
 	r := chi.NewRouter()
+	r.Use(middleware.Logger)
 	r.Get("/", homeHandler)
 	r.Get("/contact", contactHandler)
+	r.Get("/faq", faqHandler)
 
 	fmt.Println("server is ready at port 3000")
 	http.ListenAndServe(":3000", r)
