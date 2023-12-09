e4c36947fb5a0932e769fc30b69766fa330728cf
Gerry Sheva <reuss@reiss.local>
2023-12-09 16:28:01 +0700

    process signin

--- main.go
+++ main.go
@@ -37,9 +37,11 @@ func main() {
 	usersC.Templates.New = views.Must(views.ParseFS(templates.FS, "signup.gohtml", "tailwind.gohtml"))
 	usersC.Templates.SignIn = views.Must(views.ParseFS(
 		templates.FS, "signin.gohtml", "tailwind.gohtml"))
+
 	r.Get("/signup", usersC.New)
 	r.Post("/users", usersC.Create)
 	r.Get("/signin", usersC.SignIn)
+	r.Post("/signin", usersC.ProcessSignIn)
 
 	fmt.Println("server is ready at port 3000")
 	http.ListenAndServe(":3000", r)
