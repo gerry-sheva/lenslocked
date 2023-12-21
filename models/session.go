e4a896514d14b4dcc1f14b8657a79599c3c671ac
gerry-sheva <gerry.sheva@proton.me>
2023-12-21 23:51:48 +0700

    hashed the token session

--- models/session.go
+++ models/session.go
@@ -1,7 +1,9 @@
 package models
 
 import (
+	"crypto/sha256"
 	"database/sql"
+	"encoding/base64"
 	"fmt"
 
 	"github.com/gerry-sheva/lenslocked/rand"
@@ -33,12 +35,19 @@ func (ss *SessionService) Create(userID int) (*Session, error) {
 		return nil, fmt.Errorf("create session: %w", err)
 	}
 	session := Session{
-		UserID: userID,
-		Token:  token,
+		UserID:    userID,
+		Token:     token,
+		TokenHash: ss.hash(token),
 	}
 	return &session, nil
 }
 
 func (ss *SessionService) User(token string) (*User, error) {
 	return nil, nil
+}
+
+func (ss *SessionService) hash(token string) string {
+	tokenHash := sha256.Sum256([]byte(token))
+
+	return base64.URLEncoding.EncodeToString(tokenHash[:])
 }
