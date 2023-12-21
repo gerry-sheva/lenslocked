6e1ebe06b72b0a5cbbb3151a0aa780d4b1389192
Gerry Sheva <reuss@reiss.local>
2023-12-21 22:38:53 +0700

    stubbing the session models

--- models/session.go
+++ models/session.go
@@ -1,7 +1,23 @@
 package models
 
+import "database/sql"
+
 type Session struct {
 	ID        int
 	UserID    int
+	Token     string //only set when creating a new session
 	TokenHash string
+}
+
+type SessionService struct {
+	DB *sql.DB
+}
+
+func (ss *SessionService) Create(userID int) (*Session, error) {
+
+	return nil, nil
+}
+
+func (ss *SessionService) User(token string) (*User, error) {
+	return nil, nil
 }
